5cf53c4677e682c293a4a9dc0bdccda0bf2554c8
JunichiQueen <junichim@hotmail.com>
2019-08-08 11:16:56 +0100

    test

---
+++ src/test/java/com/qa/controller/AccountPrizeGenControllerTests.java
@@ -0,0 +1,30 @@
+package com.qa.controller;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import com.qa.service.AccountPrizeGenService;
+
+public class AccountPrizeGenControllerTests {
+	
+	@InjectMocks
+	AccountPrizeGenController controller;
+	
+	@Mock
+	AccountPrizeGenService service;
+	
+	@Ignore
+	@Test
+	public void getPrizeTest() {
+		
+		String reply = service.prizeDraw("333333");
+		assertEquals(controller.getPrize("333333"), reply);
+//		Mockito.when(service.prizeDraw("333333")).thenReturn("50");
+//		assertEquals(controller.getPrize("333333"), "50");
+	}
+
+}
